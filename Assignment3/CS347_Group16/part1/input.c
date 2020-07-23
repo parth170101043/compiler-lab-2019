8c8bc95a0ff6f83d7afde9ea8577226de0975a64
parth170101043 <42946384+parth170101043@users.noreply.github.com>
2020-07-23 11:06:31 +0530

    Add files via upload

---
+++ Assignment3/CS347_Group16/part1/input.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void Print();
+int test(int a);
+
+/* bjgfbvklvbukdfgb
+jdkvgbgidhg
+ghdigdiughbo
+gdvbdbvbgnl*/
+
+//gfhfhfthytutrytjurdtfh
+
+int main(){
+	//int a[7]={2,34,5},hh[9]={0,5};
+    scanf("xs%d",&s);
+    printf("d",r);
+    Print();
+    return 1;
+    for     (i=0;i<3;++i){
+        int c;
+    }}
+
+int test(){float abc;}
